c82eb8f6d902ffe4f7ed807371595970df616e94
achsahxt <achsahxt@gmail.com>
2026-04-18 21:52:33 -0500

    Delete backend/hrmanagerbackend directory

--- backend/hrmanagerbackend/README.md
+++
@@ -1,3 +0,0 @@
-# hrmanagerbackend
-
-This is the hrmanagerbackend directory.
--- backend/hrmanagerbackend/public/frontend/api/server.js
+++
@@ -1,5 +0,0 @@
-import { handleRequest } from "../request-handler.js";
-
-export default async function handler(req, res) {
-  await handleRequest(req, res, { vercel: true });
-}
