515131da99ea326dcedc6322a9016739aa95626b
achsahxt <achsahxt@gmail.com>
2026-04-20 00:05:51 -0500

    Add files via upload

--- sql files/Dump20260401.sql
+++ sql files/Dump20260401.sql
@@ -718,7 +718,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `shift`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shift` (
   `ShiftID` int NOT NULL AUTO_INCREMENT,
   `EmployeeID` int DEFAULT NULL,
